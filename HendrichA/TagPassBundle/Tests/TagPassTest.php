ec9d7c4ddf8f0e52a86f730385d919f52855ae5e
Hendrich Attila <hendricha@gmail.com>
2015-09-12 17:00:42 +0200

    Added functional tests

---
+++ HendrichA/TagPassBundle/Tests/TagPassTest.php
@@ -0,0 +1,89 @@
+<?php
+namespace HendrichA\TagPassBundle\Tests;
+
+use HendrichA\TagPassBundle\TagPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class TagPassTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTagPassShouldAddMethodCallsToConfiguredTarget()
+    {
+        $container = $this->createContainer();
+        $tagPass = new TagPass('foo');
+        $tagPass->addServiceIdsTo('mock_service', 'addFoo');
+
+        $tagPass->process($container);
+
+        $definition = $container->getDefinition('mock_service');
+        $this->assertEquals(count($definition->getMethodCalls()), 1);
+        $this->assertEquals($definition->getMethodCalls(), array(
+            array('addFoo', array(new Reference('mock_extension_Service')))
+        ));
+
+        $definition = $container->getDefinition('non_relevant_service');
+        $this->assertEquals(count($definition->getMethodCalls()), 0);
+    }
+
+    private function createContainer()
+    {
+        $container = new ContainerBuilder();
+        $container
+            ->register('mock_service');
+        $container
+            ->register('mock_extension_Service')
+            ->addTag('foo');
+        $container
+            ->register('non_relevant_service');
+
+        return $container;
+    }
+
+    public function testTagPassShouldBeAbleToAddMethodCallsToMultipleTargets()
+    {
+        $container = $this->createContainer();
+        $container
+            ->register('mock_service2');
+        $tagPass = new TagPass('foo');
+        $tagPass->addServiceIdsTo('mock_service', 'addFoo');
+        $tagPass->addServiceIdsTo('mock_service2', 'addBar');
+
+        $tagPass->process($container);
+
+        $definition = $container->getDefinition('mock_service');
+        $this->assertEquals(count($definition->getMethodCalls()), 1);
+        $this->assertEquals($definition->getMethodCalls(), array(
+            array('addFoo', array(new Reference('mock_extension_Service')))
+        ));
+        $definition = $container->getDefinition('mock_service2');
+        $this->assertEquals(count($definition->getMethodCalls()), 1);
+        $this->assertEquals($definition->getMethodCalls(), array(
+            array('addBar', array(new Reference('mock_extension_Service')))
+        ));
+
+        $definition = $container->getDefinition('non_relevant_service');
+        $this->assertEquals(count($definition->getMethodCalls()), 0);
+    }
+
+    public function testTagPassShouldAddMethodCallsBeforeConfigureCall()
+    {
+        $container = $this->createContainer();
+        $container->getDefinition('mock_service')->addMethodCall('configure');
+
+        $tagPass = new TagPass('foo');
+        $tagPass->addServiceIdsTo('mock_service', 'addFoo');
+
+        $tagPass->process($container);
+
+        $definition = $container->getDefinition('mock_service');
+        $this->assertEquals(count($definition->getMethodCalls()), 2);
+        $this->assertEquals($definition->getMethodCalls(), array(
+            array('addFoo', array(new Reference('mock_extension_Service'))),
+            array('configure', array())
+        ));
+
+        $definition = $container->getDefinition('non_relevant_service');
+        $this->assertEquals(count($definition->getMethodCalls()), 0);
+    }
+
+}
---
+++ HendrichA/TagPassBundle/Tests/TargetedTagPassTest.php
@@ -0,0 +1,86 @@
+<?php
+namespace HendrichA\TagPassBundle\Tests;
+
+use HendrichA\TagPassBundle\TargetedTagPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class TargetedTagPassTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTargetedTagPassShouldAddMethodCallsToConfiguredTarget()
+    {
+        $container = $this->createContainer(array('service' => 'mock_service'));
+        $tagPass = new TargetedTagPass('foo', 'addFoo');
+
+        $tagPass->process($container);
+
+        $definition = $container->getDefinition('mock_service');
+        $this->assertEquals(count($definition->getMethodCalls()), 1);
+        $this->assertEquals($definition->getMethodCalls(), array(
+            array('addFoo', array(new Reference('mock_extension_Service')))
+        ));
+
+        $definition = $container->getDefinition('non_relevant_service');
+        $this->assertEquals(count($definition->getMethodCalls()), 0);
+    }
+
+    private function createContainer($tagConfig)
+    {
+        $container = new ContainerBuilder();
+        $container
+            ->register('mock_service');
+        $container
+            ->register('non_relevant_service');
+        $container
+            ->register('mock_extension_Service')
+            ->addTag('foo', $tagConfig);
+
+        return $container;
+    }
+
+    public function testTargetedTagPassShouldAddMethodCallsToConfiguredTargetByTag()
+    {
+        $container = $this->createContainer(array('tag' => 'foo_target'));
+        $container->getDefinition('mock_service')->addTag('foo_target');
+        $container->register('mock_service2')->addTag('foo_target');
+
+        $tagPass = new TargetedTagPass('foo', 'addFoo');
+
+        $tagPass->process($container);
+
+        $definition = $container->getDefinition('mock_service');
+        $this->assertEquals(count($definition->getMethodCalls()), 1);
+        $this->assertEquals($definition->getMethodCalls(), array(
+            array('addFoo', array(new Reference('mock_extension_Service')))
+        ));
+
+        $definition = $container->getDefinition('mock_service2');
+        $this->assertEquals(count($definition->getMethodCalls()), 1);
+        $this->assertEquals($definition->getMethodCalls(), array(
+            array('addFoo', array(new Reference('mock_extension_Service')))
+        ));
+
+        $definition = $container->getDefinition('non_relevant_service');
+        $this->assertEquals(count($definition->getMethodCalls()), 0);
+    }
+
+    public function testTargetedTagPassShouldAddMethodCallsBeforeConfigureCall()
+    {
+        $container = $this->createContainer(array('service' => 'mock_service'));
+        $container->getDefinition('mock_service')->addMethodCall('configure');
+
+        $tagPass = new TargetedTagPass('foo', 'addFoo');
+
+        $tagPass->process($container);
+
+        $definition = $container->getDefinition('mock_service');
+        $this->assertEquals(count($definition->getMethodCalls()), 2);
+        $this->assertEquals($definition->getMethodCalls(), array(
+            array('addFoo', array(new Reference('mock_extension_Service'))),
+            array('configure', array())
+        ));
+
+        $definition = $container->getDefinition('non_relevant_service');
+        $this->assertEquals(count($definition->getMethodCalls()), 0);
+    }
+}
